7b8963f6fba83edbc5ae472c96bc4faf62e0e2b2
Mikey <mikey@Mikeys-MacBook-Air.local>
2015-11-14 19:46:53 -0800

    Started program

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+
+print "Input a file"
+num_file = open("input.txt", "r")
+
+
+#first divider 
+dividerf = num_file
+#second divider
+dividers = num_file
+#how many times this will run 
+count = num_file
+
