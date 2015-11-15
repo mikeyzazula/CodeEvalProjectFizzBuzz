692f7c2ef4e4390ae110afd0969243fa07f9958c
Mikey <mikey@Mikeys-MacBook-Air.local>
2015-11-14 20:05:10 -0800

    testing cases

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,30 @@
+#Michael Zazula 
+#Codeeval FizzBuzz
 
-print "Input a file"
-num_file = open("input.txt", "r")
+
+#print "Input a file"
+#num_file = open("input.txt", "r")
 
 
 #first divider 
-dividerf = num_file
+dividerf = 3
 #second divider
-dividers = num_file
+dividers = 5
 #how many times this will run 
-count = num_file
+count = 10
 
+
+for x in range(1,count):
+	
+	if x%dividerf == 0 && x%dividers == 0:
+		print "FB"
+
+	elif x%dividerf == 0:
+		print "F"
+
+	elif x%dividers == 0:
+		print "B" 
+
+	else:
+		print "count"
+		
