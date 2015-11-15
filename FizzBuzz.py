69579c2433f3e8379a490867a8a47235cb8961ad
Mikey <mikey@Mikeys-MacBook-Air.local>
2015-11-14 23:42:08 -0800

    successfully made a mess

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,40 +1,40 @@
-#Michael Zazula 
-#Codeeval FizzBuzz
 
-import sys 
+#Michael Zazula
+
+import sys
+
 
 def fizzbuzz(file):
-	test_file = open(file,r)
-	outList =[]
-	
-	for line in file:
-	
+   newfile = open(file, "r")
+   outList = []
 
-		#first divider 
-		dividerf = line[0]
+   for line in newfile:
+       print(newfile)
+       # first divider
+       dividerf = int(line[0])
 
-		#second divider
-		dividers = line[1]
-		
-		#how many times this will run 	
-		count = line[2]
+       # second divider
+       dividers = int(line[1])
 
-		
+       # how many times this will run
+       count = int(line[2])
 
-		for x in range(1,count+1):
-			
-			if x%dividerf == 0 and x%dividers == 0:
-				outList.append("FB")
+       for x in range(1, count + 1):
 
-			elif x%dividerf == 0:
-				outList.append("F")
+           if x % dividerf == 0 and x % dividers == 0:
+               outList.append("FB")
 
-			elif x%dividers == 0:
-				outList.append("B")
+           elif x % dividerf == 0:
+               outList.append("F")
 
-			else:
-				outList.append(x)
-		return outList
-	
-fizzbuzz(args[v1])
+           elif x % dividers == 0:
+               outList.append("B")
+
+           else:
+               outList.append(x)
+       return outList
+   newfile.close()
+
+fizzbuzz(sys.argv[1])
+
 
